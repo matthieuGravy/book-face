b50db15be16fb499e74f5f47ad6e632f15a0648e
matthieuGravy <matthieugravy@gmail.com>
2024-01-09 11:12:07 +0100

    DB connected dotenv

--- back/build/index.js
+++ back/build/index.js
@@ -5,11 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
+//Mongoose et dotenv
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const dbURI = process.env.DB_URI;
 exports.app = (0, express_1.default)();
 const connectDB = async () => {
     try {
-        await mongoose.connect("");
+        await mongoose.connect(dbURI);
     }
     catch (err) {
         console.log(err);
@@ -19,4 +23,5 @@ exports.app.use((req, _, next) => {
     console.log(req.url);
     next();
 });
+connectDB();
 exports.app.listen(4900);
--- back/src/index.ts
+++ back/src/index.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, NextFunction } from "express";
+
+//Mongoose et dotenv
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const dbURI = process.env.DB_URI;
 
 export const app: Application = express();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("");
+    await mongoose.connect(dbURI);
   } catch (err) {
     console.log(err);
   }
@@ -15,5 +20,7 @@ app.use((req: Request, _, next: NextFunction) => {
   console.log(req.url);
   next();
 });
+
+connectDB();
 
 app.listen(4900);

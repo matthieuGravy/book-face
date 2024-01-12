5b9a868dcb1b43cc251ba268b8f53046e1478843
Pressy444 <145189170+Pressy444@users.noreply.github.com>
2024-01-12 11:39:08 +0100

    ksks

---
+++ front/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer =() => {
+    return (
+        <footer className="footer items-center absolute bottom-0 bg-gray-800 mb-0 text-neutral-content h-[10vh]">
+ 
+  <img src="src\images\FoodieMedia.png" alt="" className="w-10" />
+    <p>Copyright © 2024 - All right reserved</p>
+
+ 
+</footer>
+    )
+}
+
+export default Footer
---
+++ front/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+const Profile =() => {
+    return ( 
+    
+      <h1>kzjdozihfohe</h1>
+       
+      
+      
+  );
+}
+
+    
+
+export default Profile;

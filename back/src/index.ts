d81cd1677b1f15890cbecc3ed18e7256f9ed51c3
matthieuGravy <matthieugravy@gmail.com>
2024-01-16 16:23:33 +0100

    winston pour résoudre le probleme des 500 chez les autres

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,5 +1,8 @@
 import express, { Application } from "express";
 
+const cors = require("cors");
+const winston = require("winston");
+
 import connectDB from "./config/db";
 import registerRoutes from "./routes/register";
 import authRoutes from "./routes/auth";
@@ -8,13 +11,43 @@ import profileRoutes from "./routes/profile";
 
 const app: Application = express();
 
+// Winston logger
+const { format } = winston;
+const { combine, timestamp, printf } = format;
+
+const logFormat = printf(
+  ({
+    level,
+    message,
+    timestamp,
+  }: {
+    level: string;
+    message: string;
+    timestamp: string;
+  }) => {
+    return `${timestamp} ${level}: ${message}`;
+  }
+);
+
+const logger = winston.createLogger({
+  level: "info",
+  format: combine(timestamp(), logFormat),
+  transports: [
+    new winston.transports.File({ filename: "logfile.log", level: "info" }),
+    new winston.transports.Console(),
+  ],
+});
+
+// Utilisation du logger
+logger.info("Ceci est un message d'information dans le fichier journal.");
+
 // CORS middleware
-const cors = require("cors");
 const corsOptions = {
-  origin: ["http://localhost:5218", "https://domaine.com"],
+  origin: ["http://localhost:5218"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
@@ -28,4 +61,6 @@ app.use("/login", authRoutes);
 app.use("/logout", logoutRoutes);
 app.use("/profile", profileRoutes);
 
-app.listen(4900);
+const port = process.env.PORT || 4900;
+
+app.listen(port, () => console.log(`🍿 Server running on port ${port}`));

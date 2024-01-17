544685c663648a37ae669a75cbcc743ce31bfad7
riadh-trabelsi <143900385+riadh-trabelsi@users.noreply.github.com>
2024-01-17 11:08:06 +0100

    modal

--- back/build/mail/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_mailjet_1 = __importDefault(require("node-mailjet"));
-const mj = node_mailjet_1.default.connect("a56fff46688087a19ed3679950fff80a", "42603c980a6077cba886f5a2f0d8527a");
